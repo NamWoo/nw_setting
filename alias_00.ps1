16df24bce2c393b3961a245727c3054d050a9e17
NamWoo <pre3ice@gmail.com>
2019-12-04 11:11:33 +0900

    auto

--- alias_00.ps1
+++ alias_00.ps1
@@ -1,6 +1,28 @@
+echo ""
+echo "start git repo nw_setting"
+echo ""
 cd D:\myCOM\nw_setting
-
 git add .
-
 git commit -m "auto"
 git push -u origin master
+
+echo ""
+echo "start git repo nw_shell"
+echo ""
+cd D:\myCOM\nw_shell
+git add .
+git commit -m "auto"
+git push -u origin master
+
+
+echo ""
+echo "start git repo nw_exis"
+echo ""
+cd D:\myEXIS
+git add .
+git commit -m "auto"
+git push -u origin master
+
+
+
+
