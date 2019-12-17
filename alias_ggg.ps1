63a34d9830a575e96469bad22697d05faf22005a
nw <pre3ice@gmail.com>
2019-12-17 23:18:28 +0900

    auto

--- alias_ggg.ps1
+++ alias_ggg.ps1
@@ -1,7 +1,7 @@
 echo ""
 echo "start git repo nw_setting"
 echo ""
-cd D:\!MYWorks\nw_setting
+cd D:\myCOM\nw_setting
 git add .
 git commit -m "auto"
 git push -u origin master
@@ -9,7 +9,7 @@ git push -u origin master
 echo ""
 echo "start git repo nw_shell"
 echo ""
-cd D:\!MYWorks\nw_shell
+cd D:\myCOM\nw_shell
 git add .
 git commit -m "auto"
 git push -u origin master
@@ -18,7 +18,7 @@ git push -u origin master
 echo ""
 echo "start git repo nw_exis"
 echo ""
-cd D:\!MYWorks\myEXIS
+cd D:\myEXIS
 git add .
 git commit -m "auto"
 git push -u origin master
